979ded4487a54bb04213cbe71ec4646b732fd061
sidtomar <siddhartha.tomar@gmail.com>
2026-05-08 15:47:48 +0530

    Fix vectordb auto-rebuild on Streamlit Cloud

--- setup_vectordb.py
+++ setup_vectordb.py
@@ -1,3 +1,12 @@
+import os
+from pptx import Presentation
+from lxml import etree
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
+
 DOCS_FOLDER = "./superman_docs"
 VECTOR_DB_FOLDER = "./superman_vectordb"
 splitter = RecursiveCharacterTextSplitter(
